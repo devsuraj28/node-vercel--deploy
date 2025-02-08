7720c42738a61cfff4c1103f0986b4785f5504ef
suraj.s <suraj.s@extensioncrm.com>
2025-02-08 22:24:15 +0530

    Completed Chapter 4- REST APi's & CRUD

--- index.js
+++ index.js
@@ -30,7 +30,7 @@
 // console.log(t2-t1);
 
 
-//----------------------------- Chapter 3 : Web Server -------------------------------------------------//
+//----------------------------- Chapter 2 : Web Server -------------------------------------------------//
 
 // const http = require('http');
 // const fs = require('fs');
@@ -91,7 +91,7 @@
 
 
 
-//----------------------------- Chapter 4 : Express & Middlewares -------------------------------------------------//
+//----------------------------- Chapter 3 : Express, Middlewares and REST Api's -------------------------------------------------//
 
 // const fs = require('fs');
 // const express = require('express');
@@ -107,16 +107,20 @@
 
 ///Using Middlewares....
 
+//Built-in Middlewares...
+// server.use(express.json());
+// server.use(express.static('public-folder'));
+
+///Third-Party Middlewars...
+// server.use(morgan('dev'));
+
+
 //Custom Middleware.. 
 // (Logging as Application-Level Middleware)
 // server.use((req, res, next) => {
 //     console.log(req.method, req.ip, req.hostname, new Date(), req.get('User-Agent'));
 //     next();
 // });
-
-
-///Thir-Party Middlewars...
-// server.use(morgan('dev'));
 
 //Authentication (Route-Level Middleware..)
 // const authMiddleware = (req, res, next) => {
@@ -130,10 +134,6 @@
 //         res.sendStatus(401);
 //     }
 // };
-
-//Built-in Middlewares...
-// server.use(express.json());
-// server.use(express.static('public-folder'));
 
 // Testing Api Route...  
 // server.get('/', (req, res) => {
@@ -170,3 +170,78 @@
 // server.listen(8080, (req, res) => {
 //     console.log('Server Started');
 // });
+
+
+//----------------------------- Chapter 4 : REST Api's & CRUD-------------------------------------------------//
+
+const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+
+
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products;
+
+
+const server = express();
+
+server.use(express.json());
+server.use(express.static('public-folder'));
+server.use(morgan('dev'));
+
+
+
+//REST API's - C R U D
+
+//Create Product
+server.post('/products', (req, res) => {
+    products.push(req.body);
+    res.status(201).json(req.body);
+});
+
+//Read Products
+server.get('/products', (req, res) => {
+    res.json(products);
+});
+
+//Read Product by ID
+server.get('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const product = products.find(p => p.id === id);
+    res.json(product);
+});
+
+//Update Product
+server.put('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    products.splice(productIndex, 1, { id: id, ...req.body});
+    res.status(202).json({ 'msg': "Product Updated Successfully" });
+});
+
+//Patch Product
+server.patch('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    const product = products[productIndex];
+    products.splice(productIndex, 1, {...product, ...req.body});
+    res.status(202).json({ 'data': products[productIndex], 'msg': "Product Details Updated Successfully" });
+});
+
+
+//Delete Product
+server.delete('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    const product = products[productIndex];
+    products.splice(productIndex, 1);
+    res.json({ 'data' : product,'msg': "Product Deleted Successfully" });
+});
+
+
+
+server.listen(8080, (req, res) => {
+    console.log('Server Started');
+});
+
+
