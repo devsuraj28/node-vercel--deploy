12250da44faf505aa0d9ba813e99731a4a9b3e31
suraj.s <suraj.s@extensioncrm.com>
2025-02-09 20:26:43 +0530

    Chapter 4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//----------------------------- Chapter 3 : Express, Middlewares and REST Api's -------------------------------------------------//
+//----------------------------- Chapter 4 : REST Api's & CRUD-------------------------------------------------//
 
 const fs = require('fs');
 const express = require('express');
@@ -11,69 +11,61 @@ const products = data.products;
 
 const server = express();
 
-///Using Middlewares....
-
-//Built-in Middlewares...
 server.use(express.json());
-// server.use(express.static('public-folder'));
-
-///Third-Party Middlewars...
+server.use(express.static('public-folder'));
 server.use(morgan('dev'));
 
 
-//Custom Middleware.. 
-// (Logging as Application-Level Middleware)
-// server.use((req, res, next) => {
-//     console.log(req.method, req.ip, req.hostname, new Date(), req.get('User-Agent'));
-//     next();
-// });
 
-//Authentication (Route-Level Middleware..)
-const authMiddleware = (req, res, next) => {
-    if (req.query.password == '123' && req.method == 'GET') {
-        next();
-    }
-    else if (req.body.password == '123' && req.method == 'POST') {
-        next();
-    }
-    else {
-        res.sendStatus(401);
-    }
-};
+//REST API's - C R U D
 
-// Testing Api Route...  
-// server.get('/', (req, res) => {
-// res.status(200).json(data);
-// res.send('<h1>Hello World</>');  
-// res.sendFile('D:/My Projects/node_tut/node_practice/index.html'); //Copy path and turns every backward slash to foward slash...
-// res.sendStatus(303);
-// res.json('Hello World');
-// });
+//Create Product
+server.post('/products', (req, res) => {
+    products.push(req.body);
+    res.status(201).json(req.body);
+});
 
-//Api Endpoints -- Routes --
-server.get('/', authMiddleware, (req, res) => {
-    res.json({ 'type': req.method, 'status_code': res.statusCode });
+//Read Products
+server.get('/products', (req, res) => {
+    res.json(products);
+});
+
+//Read Product by ID
+server.get('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const product = products.find(p => p.id === id);
+    res.json(product);
+});
+
+//Update Product
+server.put('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    products.splice(productIndex, 1, { id: id, ...req.body});
+    res.status(202).json({ 'msg': "Product Updated Successfully" });
+});
+
+//Patch Product
+server.patch('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    const product = products[productIndex];
+    products.splice(productIndex, 1, {...product, ...req.body});
+    res.status(202).json({ 'data': products[productIndex], 'msg': "Product Details Updated Successfully" });
 });
 
 
-server.post('/', authMiddleware, (req, res) => {
-    res.json({ 'type': req.method, 'set_status_code': res.status = 201 });
+//Delete Product
+server.delete('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    const product = products[productIndex];
+    products.splice(productIndex, 1);
+    res.json({ 'data' : product,'msg': "Product Deleted Successfully" });
 });
 
-server.put('/', (req, res) => {
-    res.json({ 'type': req.method, 'sent_status': res.sendStatus(202) });
-});
-
-server.patch('/', (req, res) => {
-    res.json({ 'type': req.method, 'status_message': res.statusMessage = 'Patch Successfull' });
-});
-
-server.delete('/', (req, res) => {
-    res.json({ 'type': req.method, 'set_headers': res.setHeaders = { 'Content-Type': 'application/json' } });
-});
 
 
 server.listen(8080, (req, res) => {
     console.log('Server Started');
 });
-
